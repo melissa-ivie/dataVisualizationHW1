c560b765f6d972644d34b57a7e9c9a4c5732973b
Melissa Ivie <>
2021-10-22 17:44:54 -0600

    finished form

--- javascript/validation.js
+++ javascript/validation.js
@@ -57,6 +57,7 @@ function testPassword (password){
 
 
 function validation(form){
+    var username, email, phone, pwd, cpwd, gender, birthday, music = false;
     document.getElementById("errors").innerHTML = " ";
     if(form.username.value == ""){
         let newcontent = document.createElement('div');
@@ -66,6 +67,8 @@ function validation(form){
         let newcontent = document.createElement('div');
         newcontent.innerHTML = '<p>Please Enter <span style="color:orange">A valid Username</span></p>';
         document.getElementById("errors").appendChild(newcontent.firstChild);
+    }else{
+        username = true;
     }
 
     if(form.email.value == ""){
@@ -76,6 +79,8 @@ function validation(form){
         let newcontent = document.createElement('div');
         newcontent.innerHTML = '<p>Please Enter <span style="color:orange">A valid Email</span></p>';
         document.getElementById("errors").appendChild(newcontent.firstChild);
+    }else {
+        email = true;
     }
 
     if(form.phone.value == ""){
@@ -86,17 +91,66 @@ function validation(form){
             let newcontent = document.createElement('div');
             newcontent.innerHTML = '<p>Please Enter <span style="color:orange">A valid Phone Number</span></p>';
             document.getElementById("errors").appendChild(newcontent.firstChild);
+    }else{
+        phone = true;
     }
 
-    // if(form.password.value == ""){
-    //     let newcontent = document.createElement('div');
-    //     newcontent.innerHTML = '<p>Please Enter <span style="color:red">Password</span></p>';
-    //     document.getElementById("errors").appendChild(newcontent.firstChild);
-    // } else if (!testPassword(form.password.value)){
-    //         let newcontent = document.createElement('div');
-    //         newcontent.innerHTML = '<p>Please Enter <span style="color:orange">A valid Password</span></p>';
-    //         document.getElementById("errors").appendChild(newcontent.firstChild);
-    // }
+    if(form.pwd.value == ""){
+        let newcontent = document.createElement('div');
+        newcontent.innerHTML = '<p>Please Enter <span style="color:red">Password</span></p>';
+        document.getElementById("errors").appendChild(newcontent.firstChild);
+     } else if (!testPassword(form.pwd.value)){
+            let newcontent = document.createElement('div');
+            newcontent.innerHTML = '<p>Please Enter <span style="color:orange">A valid Password</span></p>';
+            document.getElementById("errors").appendChild(newcontent.firstChild);
+    }else{
+        pwd = true;
+    }
+
+    if(form.cpwd.value == ""){
+        let newcontent = document.createElement('div');
+        newcontent.innerHTML = '<p>Please Enter <span style="color:red">Confirmation Password</span></p>';
+        document.getElementById("errors").appendChild(newcontent.firstChild);
+     } else if (!testPassword(form.cpwd.value)){
+            let newcontent = document.createElement('div');
+            newcontent.innerHTML = '<p>Please Enter <span style="color:orange">A valid Confirmation Password</span></p>';
+            document.getElementById("errors").appendChild(newcontent.firstChild);
+    }else{
+        cpwd = true; 
+    }
+
+    if(form.gender.value == ""){
+        let newcontent = document.createElement('div');
+        newcontent.innerHTML = '<p>Please Select a <span style="color:red">Gender</span></p>';
+        document.getElementById("errors").appendChild(newcontent.firstChild);
+    }else{
+        gender = true;
+    }
+
+    if(form.month.value == "" || form.day.value == "" || form.year.value == ""){
+        let newcontent = document.createElement('div');
+        newcontent.innerHTML = '<p>Please Select <span style="color:red">Birthday</span></p>';
+        document.getElementById("errors").appendChild(newcontent.firstChild);
+    }else{
+        birthday = true;
+    }
+
+    if(!(document.getElementById('music1').checked || document.getElementById('music2').checked || document.getElementById('music3').checked || document.getElementById('music4').checked || document.getElementById('music5').checked || document.getElementById('music6').checked)){
+        let newcontent = document.createElement('div');
+        newcontent.innerHTML = '<p>Please Select <span style="color:red">Favorite Music</span></p>';
+        document.getElementById("errors").appendChild(newcontent.firstChild);
+    }else{
+        music = true;
+    }
+
+    if(username && email && phone && pwd && cpwd && gender && birthday && music){
+        if(form.pwd.value != form.cpwd.value){
+            alert("Passwords do not match!")
+        } else {
+            location.href = '../index.html';
+        }
+    }
+
 
     return false; 
 }
